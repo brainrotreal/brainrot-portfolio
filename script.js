894049066dfaa5192a69a22b0c286cad954dcd5f
Daskey_bruhh <60326436+brainrotreal@users.noreply.github.com>
2024-08-29 22:37:04 -0300

    Scroll Animations

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 const items = document.querySelectorAll('.item');
 
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show');
+    }
+  });
+})
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
 items.forEach((item) => {
   item.addEventListener('mouseover', () => {
     document.body.classList.add('pause-animations');
